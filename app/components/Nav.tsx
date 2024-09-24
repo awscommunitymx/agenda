1369a83936cadb4d8ad35e3a011cf41ddca597bb
Sebastian Marines <hi@sebastianmarines.com>
2024-09-23 23:13:54 -0600

    Add logo

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,85 +1,16 @@
 import React from 'react';
-import {
-    Box,
-    Breadcrumb,
-    BreadcrumbItem,
-    BreadcrumbLink,
-    Button,
-    Drawer,
-    DrawerBody,
-    DrawerCloseButton,
-    DrawerContent,
-    DrawerHeader,
-    DrawerOverlay,
-    Flex,
-    IconButton,
-    Link,
-    Spacer,
-    useDisclosure,
-    VStack,
-} from '@chakra-ui/react';
-import {HamburgerIcon} from '@chakra-ui/icons';
+import {Box, Flex} from '@chakra-ui/react';
+import Image from 'next/image'
+import logo from '@/app/img.png'
 
 const Navbar = () => {
-    const {isOpen, onOpen, onClose} = useDisclosure();
-
     return (
         <Box>
-            <Box bg="gray.100" px={4}>
-                <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-                    <Box fontWeight="bold">Congress Agenda</Box>
-
-                    <Flex alignItems={'center'} display={{base: 'none', md: 'flex'}}>
-                        <Link mr={4} href="#">Schedule</Link>
-                        <Link mr={4} href="#">Speakers</Link>
-                        <Link mr={4} href="#">Venues</Link>
-
-                        <Spacer/>
-
-                        <Button colorScheme="teal" size="sm">
-                            Register
-                        </Button>
-                    </Flex>
-
-                    <IconButton
-                        size={'md'}
-                        icon={<HamburgerIcon/>}
-                        aria-label={'Open Menu'}
-                        display={{md: 'none'}}
-                        onClick={onOpen}
-                    />
+            <Box bg="gray.700" px={4}>
+                <Flex h={16} alignItems={'center'} justifyContent={'center'}>
+                    <Image src={logo} alt={"logo"} width={200}/>
                 </Flex>
             </Box>
-
-            <Breadcrumb mt={4} ml={4}>
-                <BreadcrumbItem>
-                    <BreadcrumbLink href="#">Home</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbItem>
-                    <BreadcrumbLink href="#">Agenda</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbItem isCurrentPage>
-                    <BreadcrumbLink href="#">Current Page</BreadcrumbLink>
-                </BreadcrumbItem>
-            </Breadcrumb>
-
-            <Drawer isOpen={isOpen} placement="right" onClose={onClose}>
-                <DrawerOverlay/>
-                <DrawerContent>
-                    <DrawerCloseButton/>
-                    <DrawerHeader>Menu</DrawerHeader>
-                    <DrawerBody>
-                        <VStack spacing={4} align="stretch">
-                            <Link href="#">Schedule</Link>
-                            <Link href="#">Speakers</Link>
-                            <Link href="#">Venues</Link>
-                            <Button colorScheme="teal" size="sm">
-                                Register
-                            </Button>
-                        </VStack>
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
         </Box>
     );
 };
