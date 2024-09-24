d27e4e2df178f397b18b9b82cd8ee30d5fdd4a09
Sebastian Marines <hi@sebastianmarines.com>
2024-09-24 16:10:30 -0600

    Fix static generation for favorites page

--- app/session/favorites/page.tsx
+++ app/session/favorites/page.tsx
@@ -3,11 +3,19 @@ import {Flex, Heading} from "@chakra-ui/react";
 import sessions from "@/app/data/sessions";
 import {getFavoriteSessions} from "@/app/utils/favorite";
 import SessionList from "@/app/components/SessionList";
+import {useEffect, useState} from "react";
+import {Session} from "@/app/types/session";
 
 export default function Page() {
-    const favorites = sessions.filter(
-        session => getFavoriteSessions().includes(String(session.id))
+    const [favorites, setFavorites] = useState<Session[]>([])
+
+    useEffect(() => {
+            setFavorites(sessions.filter(
+                session => getFavoriteSessions().includes(String(session.id))
+            ) || [])
+        }, []
     )
+    
     return (
         <Flex direction={"column"} alignItems={"center"}>
             <Heading>
@@ -17,3 +25,4 @@ export default function Page() {
         </Flex>
     )
 }
+
