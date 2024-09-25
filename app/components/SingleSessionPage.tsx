69cae7b60dc9031412fb4a138f13ad219f9c987a
Sebastian Marines <hi@sebastianmarines.com>
2024-09-24 19:58:45 -0600

    Add missing information to session details page
    Co-authored-by: David Victoria <90426863+davidvictoria@users.noreply.github.com>

--- app/components/SingleSessionPage.tsx
+++ app/components/SingleSessionPage.tsx
@@ -1,26 +1,52 @@
 'use client'
 
-import React from 'react';
-import {Avatar, Badge, Box, Heading, HStack, Text, VStack} from '@chakra-ui/react';
+import React, {useEffect, useState} from 'react';
+import {Avatar, Badge, Box, Button, Heading, HStack, Text, VStack} from '@chakra-ui/react';
 import {Session} from '@/app/types/session';
-import {getCategoryColor} from "@/app/utils/colors";
+import {getCategoryColor, getLevelColor} from "@/app/utils/colors";
+import {Icon} from "@chakra-ui/icons";
+import {FaClock, FaHeart, FaMapPin, FaRegHeart} from "react-icons/fa";
+import {isFavorite, toggleFavorite} from "@/app/utils/favorite";
 
 interface SingleSessionPageProps {
     session: Session;
 }
 
 const SingleSessionPage: React.FC<SingleSessionPageProps> = ({session}) => {
+    const [favorite, setFavorite] = useState(false);
+
+    const handleFavorite = () => {
+        setFavorite(!favorite);
+        toggleFavorite(session.id.toString());
+    }
+
     const formatTime = (date: Date): string => {
         return date.toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'});
     };
+
+    useEffect(() => {
+        setFavorite(
+            isFavorite(session.id.toString())
+        )
+    }, [session.id]);
 
     return (
 
         <Box borderWidth={1} borderRadius="lg" p={6} bg="white" shadow="md">
             <VStack align="stretch" spacing={4}>
-                <Badge colorScheme={getCategoryColor(session.category)} alignSelf="flex-start">
-                    {session.category}
-                </Badge>
+                <HStack justify={"space-between"}>
+                    <VStack alignItems={"start"}>
+                        <Badge colorScheme={getCategoryColor(session.category)} alignSelf="flex-start">
+                            {session.category}
+                        </Badge>
+                        <Badge colorScheme={getLevelColor(session.level)} alignSelf="flex-start">
+                            {session.level}
+                        </Badge>
+                    </VStack>
+                    <Button onClick={handleFavorite} colorScheme="red" variant="link"
+                            rightIcon={favorite ? <Icon as={FaHeart}/> : <Icon as={FaRegHeart}/>}>
+                    </Button>
+                </HStack>
 
                 <Heading as="h1" size="xl" color="blue.600">
                     {session.title}
@@ -46,13 +72,19 @@ const SingleSessionPage: React.FC<SingleSessionPageProps> = ({session}) => {
 
                 </VStack>
 
-                <Text fontSize="lg" suppressHydrationWarning>
-                    {formatTime(session.time.start)} - {formatTime(session.time.end)}
-                </Text>
+                <HStack>
+                    <Icon as={FaClock}/>
+                    <Text fontSize="lg" suppressHydrationWarning>
+                        {formatTime(session.time.start)} - {formatTime(session.time.end)}
+                    </Text>
+                </HStack>
 
-                <Text fontSize="md" color="gray.700">
-                    Room: {session.room}
-                </Text>
+                <HStack>
+                    <Icon as={FaMapPin}/>
+                    <Text fontSize="md" color="gray.700">
+                        Room: {session.room}
+                    </Text>
+                </HStack>
 
                 <Text fontSize="md">
                     {session.description}
