83fa75721941cae0c9b24dfdbb17b91c17c7fc4c
Sebastian Marines <hi@sebastianmarines.com>
2024-09-26 15:38:28 -0600

    Create session card renderer
    Co-authored-by: David Victoria <90426863+davidvictoria@users.noreply.github.com>

---
+++ app/components/SpecialSessionCard.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {SessionCardProps} from "@/app/types/session";
+
+const SpecialSessionCard: React.FC<SessionCardProps> = ({session}) => {
+    return (
+        <div>
+            Special Session Card
+        </div>
+    );
+}
+
+export default SpecialSessionCard;
