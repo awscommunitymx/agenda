c69fac00824b02db7348d8cfab0d3a597e1bce70
Sebastian Marines <hi@sebastianmarines.com>
2024-09-27 13:43:29 -0600

    Add final list of sessions
    Co-authored-by: David Victoria <90426863+davidvictoria@users.noreply.github.com>

--- app/types/session.ts
+++ app/types/session.ts
@@ -1,7 +1,7 @@
 import {IconType} from "react-icons";
 
-export type Category = 'FinOps' | 'IA/ML' | 'Operaciones' | 'PrimerosPasos' | 'Serverless' | 'Softskills';
-export type Level = 'Advanced' | 'All' | 'Beginner' | 'Intermediate';
+export type Category = 'Analítica' | 'Aplicaciones empresariales' | 'Arquitectura' | 'Bases de datos' | 'Capacitación y certificación' | 'Computación sin servidor y contenedores' | 'Conferencia magistral' | 'DevOps y productividad del desarrollador' | 'Habilidades blandas' | 'Habilitación del cliente' | 'IA generativa' | 'IA/ML' | 'Internet de las cosas' | 'Kubernetes' | 'Migración y modernización' | 'Nube híbrida' | 'Operaciones en la nube' | 'Seguridad cumplimiento e identidad';
+export type Level = 'L100' | 'L200' | 'L300' | 'L400';
 
 export type CFPLanguage = 'Spanish' | 'English';
 
@@ -41,5 +41,4 @@ export interface SessionCardProps {
 
 export interface SessionListProps {
     sessions: Session[];
-    inAgendaPage: boolean;
 }
