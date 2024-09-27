891d609a7094e64dbd5302f6c259684cfab26de0
Sebastian Marines <hi@sebastianmarines.com>
2024-09-26 18:14:53 -0600

    Add Cloudwatch RUM

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,32 @@
 import {Providers} from './providers'
 import React from "react";
 import Nav from "@/app/components/Nav";
+import {AwsRum, AwsRumConfig} from 'aws-rum-web';
 
+
+try {
+    const config: AwsRumConfig = {
+        sessionSampleRate: 1,
+        identityPoolId: "us-east-1:5f6ec45b-7055-4a9e-a04d-b4a15577e551",
+        endpoint: "https://dataplane.rum.us-east-1.amazonaws.com",
+        telemetries: ["performance", "errors", "http"],
+        allowCookies: true,
+        enableXRay: true
+    };
+
+    const APPLICATION_ID: string = 'de222605-a510-4919-90af-e12a84f71a39';
+    const APPLICATION_VERSION: string = '1.0.0';
+    const APPLICATION_REGION: string = 'us-east-1';
+
+    new AwsRum(
+        APPLICATION_ID,
+        APPLICATION_VERSION,
+        APPLICATION_REGION,
+        config
+    );
+} catch (e) {
+    console.error(e);
+}
 
 export default function RootLayout({
                                        children,
